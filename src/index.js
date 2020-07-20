f1a705ac26e329c474fec9d60d7647409a6de54b
Christian Paul Ventura <christianpaulventura@gmail.com>
2020-07-21 02:05:38 +0800

    create public directory

--- src/index.js
+++ src/index.js
@@ -6,10 +6,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
 
-app.get('/', (req, res) => {
-
-});
+app.use(express.static(publicDirectoryPath));
 
 app.listen(3000, () => {
-    console.log('app is running on port 3000');
+    console.log(`Server is up on port ${port}`);
 });
